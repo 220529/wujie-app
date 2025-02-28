7e1f11fd0c088cdb42ebc41cd53367c6ee07952e
liukaixin <15713857919@163.com>
2025-02-28 15:21:17 +0800

    init mian-app

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80, // 每行最大字符数
+  semi: true, // 是否使用分号
+  singleQuote: false, // 是否使用单引号
+  bracketSpacing: true, // 对象大括号内是否加空格
+  trailingComma: "es5", // 是否使用尾随逗号（ES5有效）
+  jsxSingleQuote: false, // JSX 中是否使用单引号
+  htmlWhitespaceSensitivity: "ignore", // HTML 中空白敏感性
+  insertPragma: false, // 是否插入格式化标记
+  requirePragma: false, // 是否要求插入格式化标记
+  tabWidth: 2, // 缩进空格数
+  useTabs: false, // 是否使用 tab 缩进
+};
